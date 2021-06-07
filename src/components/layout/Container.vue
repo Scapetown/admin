d2334e9019c4ec430d380c6b0d9c0daa8d24e13a
Martijn Faber <martijn@fabtronics.nl>
2021-06-07 17:14:11 +0200

    feat(dashboard): add container

---
+++ src/components/layout/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  width: 85%;
+  max-width: 1140px;
+  margin: 0 auto;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Container",
+});
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,15 @@
 <template>
-  <h1>hoi</h1>
+  <Container></Container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Container from "@/components/layout/Container.vue";
 
 export default defineComponent({
   name: "Dashboard",
-  components: {},
+  components: {
+    Container,
+  },
 });
 </script>
