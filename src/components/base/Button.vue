6e04d005f97fd8b50a98d6d68480341d575ace9e
Martijn Faber <martijn@fabtronics.nl>
2021-06-10 19:26:16 +0200

    feat(button): add button component

---
+++ src/components/base/Button.vue
@@ -0,0 +1,55 @@
+<template>
+  <div @click="(e) => $emit('click', e)">
+    <button class="button">
+      <slot v-if="!isLoading" />
+      <Loading :loading="isLoading"></Loading>
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  width: 100%;
+  min-height: 50px;
+  padding: 8px;
+
+  border-radius: var(--border-radius);
+  border: none;
+
+  background-color: var(--primary);
+  color: var(--text-secundair);
+
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: var(--box-shadow);
+  }
+
+  &:active {
+    opacity: 0.8;
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Loading from "@/components/base/Loading.vue";
+
+export default defineComponent({
+  name: "Button",
+  components: {
+    Loading,
+  },
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
---
+++ src/components/base/Loading.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="v-spinner" v-show="loading">
+    <div
+      class="v-pulse v-pulse1"
+      v-bind:style="[spinnerStyle, spinnerDelay1]"
+    ></div>
+    <div
+      class="v-pulse v-pulse2"
+      v-bind:style="[spinnerStyle, spinnerDelay2]"
+    ></div>
+    <div
+      class="v-pulse v-pulse3"
+      v-bind:style="[spinnerStyle, spinnerDelay3]"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Loading",
+  props: {
+    color: String,
+    loading: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      spinnerStyle: {
+        backgroundColor: this.color || "#fff",
+        width: "12px",
+        height: "12px",
+        margin: "2px",
+        borderRadius: "100%",
+        display: "inline-block",
+        animationName: "v-pulseStretchDelay",
+        animationDuration: "1s",
+        animationIterationCount: "infinite",
+        animationTimingFunction: "cubic-bezier(.2,.68,.18,1.08)",
+        animationFillMode: "both",
+      },
+      spinnerDelay1: {
+        animationDelay: "0.12s",
+      },
+      spinnerDelay2: {
+        animationDelay: "0.24s",
+      },
+      spinnerDelay3: {
+        animationDelay: "0.36s",
+      },
+    };
+  },
+});
+</script>
+
+<style>
+@-webkit-keyframes v-pulseStretchDelay {
+  0%,
+  80% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    -webkit-opacity: 1;
+    opacity: 1;
+  }
+  45% {
+    -webkit-transform: scale(0.1);
+    transform: scale(0.1);
+    -webkit-opacity: 0.7;
+    opacity: 0.7;
+  }
+}
+@keyframes v-pulseStretchDelay {
+  0%,
+  80% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    -webkit-opacity: 1;
+    opacity: 1;
+  }
+  45% {
+    -webkit-transform: scale(0.1);
+    transform: scale(0.1);
+    -webkit-opacity: 0.7;
+    opacity: 0.7;
+  }
+}
+</style>
